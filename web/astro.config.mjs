ca30cd62108d9ee20b9d7b3f932b8cd42c0de1be
Matthew Findlater <matthew.findlater@gmail.com>
2026-03-13 23:51:09 -0400

    fix build

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -6,8 +6,5 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://philasdb.org',
-  integrations: [sitemap()],
-  experimental: {
-    rustCompiler: true
-  }
+  integrations: [sitemap()]
 });
