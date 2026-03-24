b62c2561dc190bda562ca6658263966b258eda37
Matthew Findlater <matthew.findlater@gmail.com>
2026-03-23 23:07:32 -0400

    reorganize

--- apphost.cs
+++ apphost.cs
@@ -1,11 +1,11 @@
 #:package CommunityToolkit.Aspire.Hosting.Bun@13.1.2-beta.516
-#:sdk Aspire.AppHost.Sdk@13.1.1
+#:sdk Aspire.AppHost.Sdk@13.3.0-preview.1.26126.7
 
 var builder = DistributedApplication.CreateBuilder(args);
 
 var web = builder.AddBunApp(
-    name: "web-frontend",
-    workingDirectory: "./web",
+    name: "psdb-site",
+    workingDirectory: "./src/philasdb.site",
     entryPoint: "dev")
     .WithHttpEndpoint(targetPort: 4321);
 
